d0accd0ade95bf3542577da73ce8af83e47cb54e
Agrima KP <qvistn111@gmail.com>
2024-05-11 21:43:59 +1000

    Update seed.sql

---
+++ db/seed.sql
@@ -0,0 +1,19 @@
+ INSERT INTO readings
+            (time,
+             temperature,
+             humidity,
+             source)
+VALUES      ('20130623T13:22-0500',
+             34,
+             52,
+             'hall');
+
+ INSERT INTO readings
+            (time,
+             temperature,
+             humidity,
+             source)
+VALUES      ('20130623T13:30-0500',
+             34,
+             52,
+             'hall');
